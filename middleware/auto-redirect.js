f0fb83e728b4a7d6d7dac0a8adcb2c9ab79726af
Edward Nguyen <ducnguyen1504@gmail.com>
2020-10-21 03:03:48 +0700

    update index page same random page

--- middleware/auto-redirect.js
+++
@@ -1,8 +0,0 @@
-export default function (context) {
-  if (process.server) {
-    const { req, redirect } = context
-    if (req.url === '/') {
-      return redirect('/random')
-    }
-  }
-}
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,15 @@
 <template>
-  <div>
-    <nuxt-child />
+  <div class="container">
+    <!--cmty-->
+    <random-template></random-template>
   </div>
 </template>
 
 <script>
+import RandomTemplate from '@/components/templates/RandomTemplate'
 export default {
-  middleware: ['auto-redirect'],
+  components: {
+    RandomTemplate,
+  },
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
